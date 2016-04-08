c42ecb9c4ea6bb262e7a59dae5c05626c853386b
Igor Gerasymenko <libero97@yandex.ua>
2016-04-08 20:51:52 +0300

    lab 2

--- interfaceWrapper/ru/application.js
+++ interfaceWrapper/ru/application.js
@@ -6,5 +6,42 @@ function timerEvent() {
   console.log('From application timer event');
 }
 
-// Устанавливаем функцию на таймер
-setTimeout(timerEvent, 1000);
+// // Устанавливаем функцию на таймер
+// setTimeout(timerEvent, 1000);
+
+
+var fileName = './t.txt';
+
+var read = function() {
+  console.log('\nApplication going to read ' + fileName);
+  fs.readFile(fileName, function (err, src) {
+    console.log('File ' + fileName + ' size ' + src.length);
+  });
+};
+
+
+
+var st = function(){
+	console.log('\nstat function module fs demonstration:');
+	fs.stat(fileName, function(err, st){
+		// console.log(st);
+	})
+};
+
+var rd = function(){
+	console.log('\nreaddir function module fs demonstration:');
+	fs.readdir('./', function(err, rd){
+		// console.log(rd);
+	})
+};
+
+setTimeout(st, 1000);
+setTimeout(rd, 3000);
+setTimeout(read, 5000);
+
+
+// fs.rename('./READYOU.md', './README.md', function(err, src){
+// 	console.log('\nrename function module fs demonstration:');
+// 	// console.log(src);
+// })
+
--- interfaceWrapper/ru/framework.js
+++ interfaceWrapper/ru/framework.js
@@ -7,29 +7,80 @@ var fs = require('fs'),
 var context = {
   module: {},
   console: console,
+  setTimeout: setTimeout,
+  setInterval: setInterval,
+  clearInterval: clearInterval,
   // Помещаем ссылку на fs API в песочницу
-  fs: fs,
-  // Оборачиваем функцию setTimeout в песочнице
-  setTimeout: function(callback, timeout) {
-    // Добавляем поведение при вызове setTimeout
-    console.log(
-      'Call: setTimeout, ' +
-      'callback function: ' + callback.name + ', ' +
-      'timeout: ' + timeout
-    );
-    setTimeout(function() {
-      // Добавляем поведение при срабатывании таймера
-      console.log('Event: setTimeout, before callback');
-      // Вызываем функцию пользователя на событии таймера
-      callback();
-      console.log('Event: setTimeout, after callback');
-    }, timeout);
-  }
+  fs: cloneInterface(fs),
+  // // Оборачиваем функцию setTimeout в песочнице
+  // setTimeout: function(callback, timeout) {
+  //   // Добавляем поведение при вызове setTimeout
+  //   console.log(
+  //     'Call: setTimeout, ' +
+  //     'callback function: ' + callback.name + ', ' +
+  //     'timeout: ' + timeout
+  //   );
+  //   setTimeout(function() {
+  //     // Добавляем поведение при срабатывании таймера
+  //     console.log('Event: setTimeout, before callback');
+  //     // Вызываем функцию пользователя на событии таймера
+  //     callback();
+  //     console.log('Event: setTimeout, after callback');
+  //   }, timeout);
+  // }
 };
 
 // Преобразовываем хеш в контекст
 context.global = context;
 var sandbox = vm.createContext(context);
+
+var numberFunctionCalls = 0;
+var numberCallbacks = 0;
+var totalReadData = 0;
+var averageCallbacksSpeedReturning = 0;
+var time = setInterval(statisticOutput, 1000);
+
+setTimeout(function(){
+	clearInterval(time)
+}, 7000);
+
+function statisticOutput(){
+	console.log('\nNumber function calls: ' + numberFunctionCalls);
+	console.log('Number callbacks: ' + numberCallbacks);
+	console.log('Total read data: ' + totalReadData);
+	console.log('Average speed returning callbacks: ' + averageCallbacksSpeedReturning + ' callbacks / second\n');
+};
+
+function cloneInterface(anInterface) {
+  var clone = {};
+  for (var key in anInterface) {
+    clone[key] = wrapFunction(anInterface[key].name, anInterface[key]);
+  }
+  return clone;
+};
+
+function wrapFunction(fnName, fn) {
+  return function wrapper() {
+    var args = [];
+    Array.prototype.push.apply(args, arguments);
+    if(typeof(args[args.length - 1]) == typeof(Function)){
+    	numberCallbacks++;
+    	var startTime = process.hrtime();
+    	args[args.length - 1] = wrapFunction(args[args.length - 1].name, args[args.length - 1]);
+    	var endTime = process.hrtime();	
+    	var differenceTime = (endTime[1] - startTime[0]) / 1000000000;
+    	// console.log(differenceTime);
+    	averageCallbacksSpeedReturning = numberCallbacks / ((numberCallbacks - 1) * averageCallbacksSpeedReturning + differenceTime);
+    }
+    console.log('Call: ' + fnName);
+    if(args[1] instanceof Buffer){
+    	totalReadData += args[1].length;
+    }
+    console.dir(args);
+    numberFunctionCalls++;
+    fn.apply(undefined, args);
+  }
+};
 
 // Читаем исходный код приложения из файла
 var fileName = './application.js';
